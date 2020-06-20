ae7a251a1ffc040b54c1342c8f117cb7e4e27420
Thamindu <thamindu888@gmail.com>
2020-06-20 12:46:36 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sampleApp';
+  name:string = 'Thamindu';
+
+  constructor(){
+    this.name = "Kavi";
+  }
 }
