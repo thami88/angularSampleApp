cc66f6437ae91e794b90ae1d74c1ee4920cb11ef
Thamindu <thamindu888@gmail.com>
2020-06-21 00:04:05 +0530

    Hardcoded input fields change to User Input Fields

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -14,7 +14,9 @@ export class TodosComponent implements OnInit {
   constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
-    this.todos = this.todoService.getTodos();
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = this.todos;
+    });
   }
 
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,29 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../models/Todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  todoUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todoLimit ='?_limit=5';
 
-  getTodos(){
-    return [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      }
-    ]
+  constructor(private  http:HttpClient) { }
+
+  getTodos():Observable<Todo[]>{
+   return this.http.get<Todo[]>('${this.todoUrl}${this.todosLimit}');
   }
 }
